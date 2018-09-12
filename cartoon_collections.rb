4df29a541c2a268cf7d6d928c75349e81c6707a7
Amil Masic <amil.masic@gmail.com>
2018-09-12 14:54:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,9 @@ def roll_call_dwarves(dwarves)# code an argument here
   end
 end
 
-def summon_captain_planet(captains)# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.join(!)
 end
 
 def long_planeteer_calls# code an argument here
