8604b61f79c0d38e3cddaeb77ba9b05902f4e68e
Amil Masic <amil.masic@gmail.com>
2018-09-12 15:37:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,7 @@ end
 def find_the_cheese(strings)# code an argument here
     cheese_types = ["cheddar", "gouda", "camembert"]
     strings.include?(cheese_types)
-      puts "#{cheese_types[]}"
+      puts "#{cheese_types}"
   end
 # end
        # binding.pry
