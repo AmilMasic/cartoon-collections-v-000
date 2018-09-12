2ab136c348640184aaefb9d86df7c5d8cd98a454
Amil Masic <amil.masic@gmail.com>
2018-09-12 14:58:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,8 @@ end
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
   planeteer_calls.collect do |planeteer|
-    planeteer.capitalize
-    planeteer + "!"
+    planeteer.capitalize + "!"
+
   end
 end
 
